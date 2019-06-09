de4fad0dd22c81a45942f331153a2f26719edbf0
Jhonatan Froeder <froeder@unoeste.edu.br>
2019-06-09 18:02:15 -0400

    finalizado consultas

--- trabalho/consultas.sql
+++ trabalho/consultas.sql
@@ -1,5 +1,6 @@
 -- Saber quantidade de comissão por mês de cada funcionario
-SELECT p.nome, c.mes,  SUM(c.valor) AS total FROM pessoa AS p
+SELECT p.nome, c.mes,  SUM(c.valor) AS total 
+FROM pessoa AS p
 JOIN comissao AS c ON id_cliente_funcionario = p.cpf 
 GROUP BY p.nome , c.mes
 ORDER BY mes ; 
@@ -7,22 +8,24 @@ ORDER BY mes ;
 
 -- Saber qual tipo de imovel gera mais lucro para a imobiliaria por transacao
 SELECT 
-SUM(t.valor_repasse_imobiliaria) AS total, i.imovel_tipo FROM transacao AS t
+SUM(t.valor_repasse_imobiliaria) AS total, i.imovel_tipo 
+FROM transacao AS t
 JOIN imovel AS i ON i.id = t.id_imovel
 GROUP BY i.imovel_tipo
 ORDER BY total DESC;
 
 
 -- Saber quem é o proprietário dos imóveis que mais geram lucros para a imobiliaria
-SELECT 
-p.nome AS proprietario, SUM(v.valor_repasse_imobiliaria) AS total FROM v_total_repasse AS v
+SELECT p.nome AS proprietario, SUM(v.valor_repasse_imobiliaria) AS total 
+FROM v_total_repasse AS v
 INNER JOIN pessoa AS p ON v.id_cliente_proprietario = p.cpf
 GROUP BY proprietario 
 ORDER BY total DESC;
 
 
 -- Saber quais cidades e estado geram mais lucro para a imobiliaria
-SELECT e.cidade, e.estado,  SUM(t.valor_repasse_imobiliaria) AS total FROM endereco AS e , imovel AS i 
+SELECT e.cidade, e.estado,  SUM(t.valor_repasse_imobiliaria) AS total 
+FROM endereco AS e , imovel AS i 
 JOIN transacao AS t ON i.id = t.id_imovel 
 GROUP BY e.cidade , e.estado
 ORDER BY total;
@@ -33,26 +36,39 @@ SELECT tipo, SUM(valor) AS total FROM transacao
 GROUP BY tipo
 ORDER BY total DESC;
 
+
 -- Saber quais os clientes usuários que mais geram lucro para a imobiliaria
-SELECT p.nome, SUM(t.valor_repasse_imobiliaria) AS total FROM pessoa AS p
+SELECT p.nome, SUM(t.valor_repasse_imobiliaria) AS total 
+FROM pessoa AS p
 JOIN transacao AS t ON  p.cpf = t.id_cliente_usuario
 GROUP BY p.nome
 ORDER BY total DESC;
 
 
 -- Saber quem são os funcionário mais antigos na imobiliaria
-SELECT p.nome AS nome ,  EXTRACT(YEAR FROM  age(cf.data_ingresso_imobiliaria)) AS anos from pessoa AS p 
+SELECT p.nome AS nome ,  EXTRACT(YEAR FROM  age(cf.data_ingresso_imobiliaria)) AS anos 
+FROM pessoa AS p 
 JOIN cliente_funcionario AS cf ON p.cpf = cf.id_pessoa 
 ORDER BY anos desc; 
 
 
 --Saber onde está localizado os imóveis que recebem avaliações maiores que 3
-SELECT i.id , i.avaliacao AS media FROM imovel AS i 
-JOIN avaliacao AS a ON a.id_imovel = i.id 
-GROUP BY i.id, media 
-HAVING i.avaliacao > 3 
-ORDER BY media DESC;    
+SELECT e.estado , e.cidade 
+FROM endereco AS e , imovel AS i 
+WHERE i.id_endereco = e.id AND i.avaliacao > 3
+GROUP BY e.estado , e.cidade ;
 
 
 -- Saber quem são os proprietários dos imóveis que tem média de avaliação menores que 3 ;
+SELECT p.nome AS nome 
+FROM pessoa AS p , imovel AS i  , cliente_proprietario AS cp
+WHERE p.cpf = cp.id_pessoa AND i.avaliacao < 3 AND i.avaliacao > 0
+GROUP BY nome   ;
 
+
+-- Saber lucro gerado por forma de pagamento
+SELECT fp.descricao , SUM(t.valor_repasse_imobiliaria) AS repasse 
+FROM forma_pagamento AS fp
+INNER JOIN transacao AS t ON fp.id = t.id_forma_pagamento
+GROUP BY fp.descricao 
+ORDER BY repasse DESC;
--- trabalho/esquema.sql
+++ trabalho/esquema.sql
@@ -206,10 +206,152 @@ CREATE TABLE avaliacao(
 );
 
 
---MAPEMANETO DE TABELA
+-- Triggers para atender alguns requisitos
 
---ARRUMAR RELACIONAMENTO TOTAL EM ENTIDADE FRACAS DE IMOVEL PRO GENERALIZACAO
+-- Para atribuir o salario do funcionario pelo id
+CREATE OR REPLACE FUNCTION fn_atribui_salario() RETURNS TRIGGER AS $$
+DECLARE salario_temp REAL := 0.0 ;
+BEGIN 
+    SELECT INTO salario_temp salario_base FROM cargo where id = new.id_cargo ;
+ 	UPDATE cliente_funcionario SET salario = salario_temp WHERE id_pessoa = NEW.id_pessoa ;
+    RETURN NEW ;
+END ;
 
---tipo de dados do postgres no dicionario de dados
+$$ language 'plpgsql' ;
 
---fiador e indicacao tem q ser unico
+CREATE TRIGGER tr_atribui_salario
+AFTER INSERT ON cliente_funcionario
+FOR EACH ROW
+EXECUTE PROCEDURE fn_atribui_salario() ;
+
+
+-- Para atualizar o salario para cada transacao do funcionario
+CREATE OR REPLACE FUNCTION fn_atualiza_salario() RETURNS TRIGGER AS $$
+BEGIN 
+    UPDATE cliente_funcionario SET salario = salario + NEW.valor_comissao WHERE id_pessoa = NEW.id_cliente_funcionario;
+    RETURN NEW ;
+END ;
+$$ language 'plpgsql';
+
+CREATE TRIGGER tr_atualiza_salario 
+AFTER INSERT OR UPDATE ON transacao 
+FOR EACH ROW
+EXECUTE PROCEDURE fn_atualiza_salario() ;
+
+
+-- Para atualizar o valor real do imovel
+
+CREATE OR REPLACE FUNCTION fn_atualizar_valor_imovel() RETURNS TRIGGER AS $$
+BEGIN 
+    UPDATE imovel SET valor_real = new.valor WHERE id = new.id_imovel ;
+    RETURN NEW ;
+END;
+
+$$language 'plpgsql' ;
+
+CREATE TRIGGER tr_atualiza_valor_imovel
+AFTER INSERT OR UPDATE ON transacao
+FOR EACH ROW
+EXECUTE PROCEDURE fn_atualizar_valor_imovel() ;
+
+-- PARA CRIAR TABELA HISTORICO TRANSACAO
+CREATE OR REPLACE FUNCTION fn_tabela_historico_imovel() RETURNS TRIGGER AS $$
+BEGIN
+    IF( TG_OP = 'INSERT') THEN
+        INSERT INTO historico_imovel(id_cliente_funcionario, id_imovel, data_colocado) VALUES (new.id_cliente_funcionario, new.id_imovel, now()) ;
+    END IF ;
+    IF (TG_OP = 'UPDATE') THEN
+        INSERT INTO historico_imovel(id_cliente_funcionario, id_imovel, data_alteracao) VALUES (new.id_cliente_funcionario, new.id_imovel, now()) ;
+    END IF ;
+    RETURN NEW ;
+END ;
+$$ language 'plpgsql' ;
+
+CREATE TRIGGER tr_cria_historico_imovel
+AFTER INSERT OR UPDATE ON transacao
+FOR EACH ROW
+EXECUTE PROCEDURE fn_tabela_historico_imovel() ;
+
+-- Para atualizar o tipo e a disponibilidade
+
+CREATE OR REPLACE FUNCTION fn_atualiza_tipo() RETURNS TRIGGER AS $$
+BEGIN 
+    UPDATE imovel SET disponivel = false WHERE id = NEW.id_imovel ;
+    UPDATE imovel SET disponivel_tipo = NEW.tipo WHERE id = NEW.id_imovel ;
+    RETURN NEW ;
+END ;
+$$ language 'plpgsql' ;
+
+
+CREATE TRIGGER tr_atualiza_tipo
+AFTER INSERT OR UPDATE ON transacao
+FOR EACH ROW
+EXECUTE PROCEDURE fn_atualiza_tipo() ; 
+
+-- Para atualizar a tabela comissao
+
+CREATE OR REPLACE FUNCTION fn_atualiza_comissao() RETURNS TRIGGER AS $$
+BEGIN 
+     INSERT INTO comissao(id_cliente_funcionario, id_transacao, mes, valor) VALUES(NEW.id_cliente_funcionario, new.id_numero_contrato, EXTRACT(MONTH FROM new.data_transacao), new.valor_comissao) ;
+     RETURN NEW ;
+END ;
+
+$$ language 'plpgsql' ;
+
+CREATE TRIGGER tr_atualiza_tabela_comissao
+AFTER INSERT OR UPDATE ON transacao
+FOR EACH ROW
+EXECUTE PROCEDURE fn_atualiza_comissao() ;
+
+
+-- Para atualizar repasse imobiliaria
+CREATE OR REPLACE FUNCTION fn_atualiza_repasse() RETURNS TRIGGER AS $$
+    DECLARE repasse real := 0.0 ;
+    BEGIN 
+        SELECT INTO repasse valor_repasse_imobiliaria FROM imovel WHERE id = NEW.id_imovel ;
+        UPDATE transacao SET valor_repasse_imobiliaria = (new.valor / 100) * repasse WHERE id_imovel = NEW.id_imovel;
+    RETURN NEW ;
+    END ;
+$$ language 'plpgsql' ;
+
+CREATE TRIGGER tr_atualiza_repasse
+AFTER INSERT ON transacao 
+FOR EACH ROW
+EXECUTE PROCEDURE fn_atualiza_repasse() ;
+
+
+-- Criar View para passar total real do repasse 
+CREATE OR REPLACE VIEW v_total_repasse AS
+SELECT valor_repasse_imobiliaria, id_cliente_proprietario FROM transacao GROUP BY valor_repasse_imobiliaria, id_cliente_proprietario ORDER BY valor_repasse_imobiliaria DESC;
+
+-- View para fazer a media das avaliacoes
+CREATE OR REPLACE VIEW v_media_avaliacao AS
+SELECT ((nota_questao_1 + nota_questao_2 + nota_questao_3 + nota_questao_4 + nota_questao_5) / 5 ) AS media_parcial, id_imovel FROM avaliacao ;
+
+SELECT * FROM v_media_avaliacao ;
+
+
+-- Trigger para atualizar média das avaliações
+CREATE OR REPLACE FUNCTION fn_atualiza_media_avaliacao() RETURNS TRIGGER AS $$
+    DECLARE media_view real := 0.0 ;
+    DECLARE media_imovel real := 0.0 ;
+    DECLARE media_total real := 0.0 ;
+    BEGIN
+        SELECT INTO media_view v.media_parcial FROM v_media_avaliacao AS v WHERE v.id_imovel = NEW.id_imovel ;
+        SELECT INTO media_imovel i.avaliacao FROM imovel AS i WHERE i.id = NEW.id_imovel ;
+        IF(media_imovel = 0.0) THEN
+            UPDATE imovel SET avaliacao = media_view WHERE id = NEW.id_imovel ;
+        END IF;
+        IF(media_imovel > 0.0) THEN
+            media_total = (media_view + media_imovel) / 2 ;
+            UPDATE imovel SET avaliacao = media_total WHERE id = NEW.id_imovel ;
+        END IF;
+        RETURN NEW ;
+    END ;
+$$ language 'plpgsql' ;
+
+CREATE TRIGGER tr_atualiza_media
+AFTER INSERT ON avaliacao
+FOR EACH ROW
+EXECUTE PROCEDURE fn_atualiza_media_avaliacao() ;
+
